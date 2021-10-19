ddcb03d66edf6550a8698736e9b78d9bc26353a3
murarikumar57 <murarikumar57@gmail.com>
2021-10-19 18:32:53 +0530

    front page 5 items setup with css

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/Directory";
+import "./homepage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="hompage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
