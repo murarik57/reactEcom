1da95e50f07b4b326762faa75aa2d3547240a155
murarikumar57 <murarikumar57@gmail.com>
2021-10-26 19:22:44 +0530

    learn to use redux-persist to store data locally

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
-const store = createStore(
+
+export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
-export default store;
+
+export const persistor = persistStore(store);
